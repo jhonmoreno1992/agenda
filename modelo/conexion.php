f8aacb0992f1c7e0a5d9cc47bf38efa8bb049f0a
jhonmoreno1992 <2520221010@coreducacion.edu.co>
2024-10-19 11:30:55 -0500

    se agrego el archivo conexion.php

---
+++ modelo/conexion.php
@@ -0,0 +1,26 @@
+<?php
+
+class Conexion{
+    private $host = 'localhost';
+    private $db_name = 'agenda';
+    private $username = 'root';
+    private $password = 'Jhon2772';
+    public $conn;
+
+    public function getConexion(){
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}",$this->username,$this->password);
+            $this->conn->exec("set names UTF8");
+        } catch (PDOException $execption){
+            echo "Error de Conexion".$exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+
+
+}
+
+?>
